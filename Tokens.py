c3841665f6346a20ceab94d19a363aca88026e3e
Denil <Denil1@bk.ru>
2025-11-13 01:18:17 +0300

    добавление разбиения мат. выражения на токены и пробела в шаблон токена

--- Tokens.py
+++ Tokens.py
@@ -1,10 +1,21 @@
 import re
 
+# Шаблоны токенов
 Patterns = re.compile(
     r"""
     (?P<NUMBER>-?\d+(\.\d+)?)   |
     (?P<OPERATORS>[+\-*/])      |
     (?P<LEFT_BRACKET>\()        |
-    (?P<RIGHT_BRACKET>\))
+    (?P<RIGHT_BRACKET>\))       |
+    (?P<SPACE>\s+)
     """, re.VERBOSE
 )
+
+# Разбиение математического выражения на токены
+def tokenize(text):
+    tokens = []
+    for match in Patterns.finditer(text):
+        if match.lastgroup == "SPACE":
+            continue
+        tokens.append((match.lastgroup, match.group()))
+    return tokens
